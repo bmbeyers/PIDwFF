880a7730cb989252bd2dedfefbe6fef5ce58c8ba
Brandon Beyers <36121020+bmbeyers@users.noreply.github.com>
2019-06-28 17:32:36 -0500

    Added source files

---
+++ PIDwFF.cpp
@@ -0,0 +1,175 @@
+/******************************************************************************
+ * Arduino PID with Feed-Forward Controller Library                           *
+ * by Brandon M Beyers <bmbeyers@gmail.com>                                   *
+ * original source Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com    *
+ *                                                                            *
+ * This library is licensed under a GPLv3 license.                            *
+ *                                                                            *
+ * Refer to header file for additional information on PID controller.         *
+ ******************************************************************************/
+
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include <PIDwFF.h>
+
+/* Public functions ***********************************************************/
+PID::PID(double* Setpoint, double* Input, double* Output, bool Inverse) {
+  _setpoint = Setpoint;
+  _input = Input;
+  _output = Output;
+  PID::setInverse(Inverse);
+}
+
+PID::PID(double* Setpoint, double* Input, double* Output)
+  :PID::PID(Setpoint, Input, Output, false) {}
+
+void PID::setProportional(double Kp) { PID::setTuning(_userKp, Kp); }
+
+void PID::setIntegral(double Ki) { PID::setTuning(_userKi, Ki); }
+
+void PID::setDerivative(double Kd) { PID::setTuning(_userKd, Kd); }
+
+void PID::setFeedForward(double Kf) { PID::setTuning(_userKf, Kf); }
+
+void PID::setSampleTime(int NewSampleTime) {
+  if ( NewSampleTime < MIN_SAMPLE_TIME || _enabled ) return;
+  _sampleTime = (unsigned long)NewSampleTime;
+}
+
+void PID::setOutputLimits(double min, double max) {
+  if ( max >= min ) {
+    _minOutputLimit = min;
+    _maxOutputLimit = max;
+  } else {  // handle scenario where limits are provided backwards
+    _minOutputLimit = max;
+    _maxOutputLimit = min;
+  }
+  _outputLimitEnabled = true;
+}
+
+void PID::setNoOutputLimits() { _outputLimitEnabled = false; }
+
+void PID::setIntegratorLimits(double min, double max) {
+  if ( max >= min ) {
+    _minIntegratorLimit = min;
+    _maxIntegratorLimit = max;
+  } else {  // handle scenario where limits are provided backwards
+    _minIntegratorLimit = max;
+    _maxIntegratorLimit = min;
+  }
+  _integratorLimitEnabled = true;
+}
+
+void PID::setNoIntegratorLimits() { _integratorLimitEnabled = false; }
+
+void PID::setInverse(bool Inverse) { _inverted = Inverse; }
+
+void PID::enableController() {
+  if ( !PID::validTuning() ) return;
+  if ( !_enabled ) PID::initializeController();
+  _enabled = true;
+}
+
+void PID::disableController() { _enabled = false; }
+
+bool PID::compute() {
+  /* This is, as they say, where the magic happens. This function should be
+   * called every time "void loop()" executes. The function will decide for
+   * itself whether a new PID output needs to be computed, and will return true
+   * when it has done so; otherwise, it will return false. */
+  if ( !_enabled ) return false;
+
+  unsigned long timeNow = millis();
+  unsigned long timeChange = (timeNow - _lastTime);
+  if ( timeChange >= _sampleTime ) {
+    _lastTime = timeNow;
+    double input = *_input;
+    double error = *_setpoint - input;
+    double deltaError = (error - _lastError);
+    _lastError = error;
+
+    _KpOut = _Kp * error;
+
+    _KiOut += _Ki * error;
+    if ( _integratorLimitEnabled ) {
+      if ( _KiOut > _maxIntegratorLimit ) _KiOut = _maxIntegratorLimit;
+      if ( _KiOut < _minIntegratorLimit )  _KiOut = _minIntegratorLimit;
+    }
+
+    _KdOut = _Kd * deltaError;
+
+    _KfOut = _Kf * input;
+
+    double Output = _KpOut + _KiOut + _KdOut + _KfOut;
+
+    if ( _inverted ) Output = -Output;
+    if ( _outputLimitEnabled ) {
+      if ( Output > _maxOutputLimit ) Output = _maxOutputLimit;
+      if ( Output < _minOutputLimit ) Output = _minOutputLimit;
+    }
+
+	  *_output = Output;
+	  return true;
+  } else return false;
+}
+
+double PID::getKp() { return _userKp; }
+double PID::getKi() { return _userKi; }
+double PID::getKd() { return _userKd; }
+double PID::getKf() { return _userKf; }
+
+double PID::getKpOutput() { return _KpOut; }
+double PID::getKiOutput() { return _KiOut; }
+double PID::getKdOutput() { return _KdOut; }
+double PID::getKfOutput() { return _KfOut; }
+
+bool PID::getEnabledStatus() { return _enabled; }
+bool PID::getInverseStatus() { return _inverted; }
+
+bool PID::getOutputLimitStatus() { return _outputLimitEnabled; }
+double PID::getOutputMinLimit() { return _minOutputLimit; }
+double PID::getOutputMaxLimit() { return _maxOutputLimit; }
+
+bool PID::getIntegratorLimitStatus() { return _integratorLimitEnabled; }
+double PID::getIntegratorMinLimit() { return _minIntegratorLimit; }
+double PID::getIntegratorMaxLimit() { return _maxIntegratorLimit; }
+
+int PID::getSampleTime() { return _sampleTime; }
+double PID::getSampleTimeInSeconds() {
+  return (double)PID::getSampleTime() / 1000.0; }
+
+/* Private functions **********************************************************/
+void PID::setTuning(double &userGain, double value) {
+  if ( value < MIN_GAIN ) return;
+  userGain = value;
+  if ( _enabled ) setControllerTuning();
+}
+
+void PID::initializeController() {
+  PID::setControllerTuning();
+  /* Do as much as you can to ensure a bumpless transfer: */
+  _KiOut = *_output;
+  _lastError = *_setpoint - *_input;
+  _lastTime = (unsigned long)millis();
+}
+
+bool PID::validTuning() {
+  /* Cannot have any gains be negative: */
+  if ( _userKp < 0.0 || _userKi < 0.0 || _userKd < 0.0 || _userKf < 0.0 ) {
+    return false;
+  }
+  /* Need at least Kp or Ki to be non-zero: */
+  if ( _userKp > 0.0 || _userKi > 0.0 ) { return true; } else { return false; }
+}
+
+void PID::setControllerTuning() {
+  if ( !PID::validTuning() ) { PID::disableController(); return; }
+  _Kp = _userKp;
+  _Ki = _userKi * PID::getSampleTimeInSeconds();
+  _Kd = _userKd / PID::getSampleTimeInSeconds();
+  _Kf = _userKf;
+}
---
+++ PIDwFF.h
@@ -0,0 +1,186 @@
+/******************************************************************************
+ * Arduino PID with Feed-Forward Controller Library                           *
+ * by Brandon M Beyers <bmbeyers@gmail.com>                                   *
+ * original source Brett Beauregard <br3ttb@gmail.com> brettbeauregard.com    *
+ *                                                                            *
+ * This library is licensed under a GPLv3 license.                            *
+ *                                                                            *
+ *                                                                            *
+ * The common PID Controller is implemented in a closed feedback loop and has *
+ * a maximum of three terms: the proportional (P) term, the integral (I)      *
+ * term, and the derivative (D) term. It is also common to have controllers   *
+ * with one or two of these terms set to zero, resulting in a simpler type    *
+ * controller, such as P, I, PI, or PD controllers. The controller acts on    *
+ * the measured error between Input parameter value and Setpoint parameter    *
+ * value. Improperly tuned PID controllers can demonstrate unstable behavior, *
+ * which could lead to equipment damage. For this reason, use of this         *
+ * controller should be used only by competent individuals.                   *
+ *                                                                            *
+ * The additional feed-forward control is an open-loop gain based on the      *
+ * Setpoint parameter value, which may be used to improve the transient       *
+ * response of the controller. This optional feature requires fundamental     *
+ * knowledge of the steady-state relationship between Setpoint and Output,    *
+ * which may not be consistent across all modes of operation. For this reason,*
+ * use of this controller should be used only by competent individuals.       *
+ *                                                                            *
+ * The effective block diagram of this controller is shown below:             *
+ *                                                                            *
+ *                                                                            *
+ *                                    +----+ Of                               *
+ *            +---------------------->| Kf |-----+                            *
+ *            |                       +----+     |                            *
+ *            |                                  |                            *
+ *            |                            _Imax |                            *
+ *            |                           /      |                            *
+ *            |                       +----+     |                            *
+ *            |                       | Ki | Oi  |   +---+                    *
+ *            |                 +---->| -- |--+  |   |   |                    *
+ *            |                 |     |  s |  |  +-->| + |                    *
+ *            |   +---+         |     +----+  |      |   |         _Omax      *
+ *            |   |   |         | Imin_/      +----->| + |        /           *
+ * Setpoint --+-->| + |         |     +----+ Op      |   |------------>Output *
+ *                |   |--error--+---->| Kp |-------->| + | Omin_/             *
+ *    Input ----->| - |         |     +----+         |   |                    *
+ *                |   |         |                +-->| + |                    *
+ *                +---+         |   +-------+    |   |   |                    *
+ *                              |   |  sKd  | Od |   +---+                    *
+ *                              +-->| ----- |----+                            *
+ *                                  | 1+sTd |                                 *
+ *                                  +--------                                 *
+ *                                                                            *
+ *                                                                            *
+ * NOTE: Derivative block time constant (Td) is set implicitly based on the   *
+ * chosen SampleTime setting.                                                 *
+ ******************************************************************************/
+
+#ifndef PIDwFF_h
+#define PIDwFF_h
+
+class PID {
+  public:
+    /* Constructor: Links the PID controller with the Input, Output, and
+     * Setpoint pointers; allows the user to invert the output. PID and feed-
+     * forward gains are all initialized at zero, and must be set manually
+     * before the controller can be enabled. */
+    PID(double* Setpoint, double* Input, double* Output, bool Inverse);
+
+    /* Overloaded constructor: Inverse automatically set to false. */
+    PID(double* Setpoint, double* Input, double* Output);
+
+    /* Set the PID and feed-forward tuning values: */
+    void setProportional(double Kp);
+    void setIntegral(double Ki);
+    void setDerivative(double Kd);
+    void setFeedForward(double Kf);
+
+    /* Set the sample time:
+     * NOTE: Only allowed when controller is not enabled. */
+    void setSampleTime(int NewSampleTime);
+
+    /* Set the minimum and maximum limits for Output and Integrator: */
+    void setOutputLimits(double min, double max);
+    void setNoOutputLimits();
+    void setIntegratorLimits(double min, double max);
+    void setNoIntegratorLimits();
+
+    /* Invert the calculated output: */
+    void setInverse(bool Inverse);
+
+    /* Enables the controller to compute Output; initializes when enabled: */
+    void enableController();
+    void disableController();
+
+    /* Performs the PID calculation; returns true if output is computed:
+     * NOTE: This should be called every time loop() cycles. */
+    bool compute();
+
+    /* Display functions: These functions query the PID controller for the
+     * internally-used values. It is intended for front-end use, where the user
+     * needs to know what values are actually being used inside the control. */
+  	double getKp();
+  	double getKi();
+  	double getKd();
+    double getKf();
+
+    double getKpOutput();
+    double getKiOutput();
+    double getKdOutput();
+    double getKfOutput();
+
+  	bool getEnabledStatus();
+	  bool getInverseStatus();
+
+    bool getOutputLimitStatus();
+    double getOutputMinLimit();
+    double getOutputMaxLimit();
+
+    bool getIntegratorLimitStatus();
+    double getIntegratorMinLimit();
+    double getIntegratorMaxLimit();
+
+    int getSampleTime();
+    double getSampleTimeInSeconds();
+
+  private:
+    /* Generic function to set tuning gain: */
+    void setTuning(double &userGain, double value);
+
+    /* Initialize the controller using the user-provided gains and settings: */
+	  void initializeController();
+
+    /* Validation check of the user-provided gains: */
+    bool validTuning();
+
+    /* Convert user PID settings into those used by the contorller: */
+    void setControllerTuning();
+
+    /* Minimum tuning gains and time constant settings: */
+    const double MIN_GAIN = 0.0;
+    const int MIN_SAMPLE_TIME = 5;
+
+    /* User-provided tuning gain values: */
+	  double _userKp = MIN_GAIN;
+	  double _userKi = MIN_GAIN;
+	  double _userKd = MIN_GAIN;
+    double _userKf = MIN_GAIN;
+
+    /* Calculated tuning gain values set during the initialization process: */
+	  double _Kp;
+    double _Ki;
+    double _Kd;
+    double _Kf;
+
+    /* Stored PID outputs: */
+    double _KpOut;
+    double _KiOut;
+    double _KdOut;
+    double _KfOut;
+
+    /* Stored derivative integrator, required for derivative gain: */
+    double _lastError;
+
+    /* Output and Integrator limit settings: */
+    double _minOutputLimit = 0.0;
+    double _maxOutputLimit = 255.0;
+    double _minIntegratorLimit = 0.0;
+    double _maxIntegratorLimit = 255.0;
+
+    /* Boolean settings: */
+	  bool _inverted;
+	  bool _enabled = false;
+    bool _outputLimitEnabled = false;
+    bool _integratorLimitEnabled = false;
+
+    /* Pointers to the Input, Output, and Setpoint variables:
+     * This creates a hard link between the variables and the PID controller,
+     * freeing the user from having to constantly tell us what these values are.
+     * With pointers, the controller just accesses it automatically. */
+    double *_input;
+    double *_output;
+    double *_setpoint;
+
+    /* Timing Variables: */
+    unsigned long _sampleTime;
+	  unsigned long _lastTime;
+};
+#endif
